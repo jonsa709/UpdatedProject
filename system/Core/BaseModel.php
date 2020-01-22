3c324d8cab42706e7b7b290315a6fe8515802851
Abhi <shaabhi699@gmail.com>
2020-01-22 13:24:29 +0545

    Day 5

--- system/Core/BaseModel.php
+++ system/Core/BaseModel.php
@@ -10,9 +10,11 @@ abstract class BaseModel
     protected $table;
     protected $pk = 'id';
     protected $db;
-
+    protected $orderBy;
     protected $select = '*';
     protected $conditions;
+    protected $offset = 0;
+    protected $limit;
 
     public function __construct()
     {
@@ -24,32 +26,56 @@ abstract class BaseModel
     return $this->table;
 }
 
-public function getPk()
-{
-    return $this->pk;
-}
-
-public function select($columns)
-{
-    $this->select = $columns;
-
-    return $this;
-}
-
-public function where($column, $value, $operators = '=')
-{
-    if(empty($this->conditions)){
-        $this->conditions = "{$column} {$operators} '{$value}'";
+    public function getPk()
+    {
+        return $this->pk;
     }
-    else {
-        $this->conditions .= " AND {$column} {$operators} '{$value}'";
-    }
-    return $this;
-}
-public function orWhere($column, $value, $operator = '=')
-{
-    $this->conditions .=" OR {$column} {$operator} '{$value}'";
 
-    return $this;
-}
+    public function select($columns)
+    {
+        $this->select = $columns;
+
+        return $this;
+    }
+
+    public function where($column, $value, $operators = '=')
+    {
+        if(empty($this->conditions)){
+            $this->conditions = "{$column} {$operators} '{$value}'";
+        }
+        else {
+            $this->conditions .= " AND {$column} {$operators} '{$value}'";
+        }
+        return $this;
+    }
+    public function orWhere($column, $value, $operator = '=')
+    {
+        $this->conditions .=" OR {$column} {$operator} '{$value}'";
+
+        return $this;
+    }
+
+    public function orderBy($column, $direction = "ASC")
+    {
+        if(empty($this->orderBy)){
+            $this->orderBy ="{$column} {$direction}";
+        }
+        else {
+            $this->orderBy .=", {$column} {$direction}";
+        }
+
+        return $this;
+    }
+
+    public function offset($value)
+    {
+        $this->offset = $value;
+        return $this;
+    }
+
+    public function limit($value)
+    {
+        $this->limit = $value;
+        return $this;
+    }
 }
