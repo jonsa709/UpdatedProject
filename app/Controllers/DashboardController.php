d2389fe4ca64aa53a081ce72d1a91d8a0665e97c
newssite <khadkasuraj6584@gmail.com>
2020-02-03 13:13:24 +0545

    starting of project-newssite

---
+++ app/Controllers/DashboardController.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Controllers;
+
+
+use System\Core\BaseController;
+
+class DashboardController extends BaseController
+{
+
+    public function index()
+    {
+    view('cms/dashboard/index.php');
+    }
+
+}
