41c33435cfed2a8584e24ed995b64490bcfa214a
newssite <khadkasuraj6584@gmail.com>
2020-02-20 13:35:07 +0545

    starting of project newssite day 13 article

---
+++ app/Controllers/ArticlesController.php
@@ -0,0 +1,85 @@
+<?php
+
+
+namespace App\Controllers;
+
+use App\Models\Article;
+use System\Core\BaseController;
+
+
+class ArticlesController extends BaseController
+{
+    public function __construct()
+    {
+        auth(url('login'));
+
+    }
+    public function index()
+    {
+        $article = new Article();
+        $paginate = $article->paginate();
+
+        $articles = $paginate['data'];
+
+        view('cms/articles/index.php', compact('articles','paginate'));
+    }
+    public function create()
+    {
+        view('cms/articles/create.php');
+
+    }
+
+    public function store()
+    {
+        extract($_POST);
+
+        $article = new Article();
+        $article->name = $name;
+        $article->slug = $slug;
+        $article->content = $content;
+        $article->featured_image = $featured_image;
+        $article->category_id = $category_id;
+        $article->status = $status;
+        $article->created_at = date('Y-m-d H:i:s');
+        $article->updated_at = date('Y-m-d H:i:s');
+        $article->published_at = date('Y-m-d H:i:s');
+        $article->save();
+
+        set_message('article added.', 'success');
+        redirect(url('articles'));
+    }
+
+    public function edit($id)
+    {
+        $category = new Article($id);
+
+        view('cms/articles/edit.php', compact('article'));
+    }
+
+    public function update($id)
+    {
+        $article = new Article($id);
+
+        extract($_POST);
+        $article->name = $name;
+        $article->slug = $slug;
+        $article->content = $content;
+        $article->featured_image = $featured_image;
+        $article->category_id = $category_id;
+        $article->status = $status;
+        $article->updated_at = date('Y-m-d H:i:s');
+        $article->published_at = date('Y-m-d H:i:s');
+        $article->save();
+
+        set_message('article updated.', 'success');
+        redirect(url('articles'));
+    }
+
+    public function destroy($id){
+        $article = new Article($id);
+        $article->delete();
+
+        set_message('Article removed.','success');
+        redirect(url('Articles'));
+    }
+}
